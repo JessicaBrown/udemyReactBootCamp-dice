9997794156c30241d87941b4ba30c989dc5f3568
Jess <jessica.brown@tbsokc.com>
2022-07-13 14:08:44 -0500

    refactored-removed switch and updated logic to use state more efficiently

--- src/Die.js
+++ src/Die.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./Die.css";
 
-// class Die extends Component {
 export const Die = ({ face, rolling }) => {
-	console.log("face", face, rolling);
 
 	return <i className={`Die fas fa-dice-${face} ${rolling && "shaking"}`} />;
 };
--- src/RollDice.js
+++ src/RollDice.js
@@ -3,79 +3,32 @@ import { Die } from "./Die";
 import "./RollDice.css";
 
 export const RollDice = () => {
-	const [die1, setDie1] = useState("one");
-	const [die2, setDie2] = useState("one");
-	const [rolling, setRolling] = useState(false);
-
 	const sides = ["one", "two", "three", "four", "five", "six"];
+	let [dice, setDice] = useState({
+		dieOne: "one",
+		dieTwo: "one",
+		rolling: false,
+	});
 
-	const roll = () => {
-		let die1 = Math.floor(Math.random() * sides.length);
-		switch (die1) {
-			case 0:
-				die1 = "one";
-				break;
-			case 1:
-				die1 = "two";
-				break;
-			case 2:
-				die1 = "three";
-				break;
-			case 3:
-				die1 = "four";
-				break;
-			case 4:
-				die1 = "five";
-				break;
-			case 5:
-				die1 = "six";
-				break;
-			default:
-				die1 = "one";
-				break;
-		}
-
-		let die2 = Math.floor(Math.random() * sides.length);
-		switch (die2) {
-			case 0:
-				die2 = "one";
-				break;
-			case 1:
-				die2 = "two";
-				break;
-			case 2:
-				die2 = "three";
-				break;
-			case 3:
-				die2 = "four";
-				break;
-			case 4:
-				die2 = "five";
-				break;
-			case 5:
-				die2 = "six";
-				break;
-			default:
-				die2 = "one";
-				break;
-		}
-
-		setDie1(die1);
-		setDie2(die2);
-		setRolling(true);
-
+	const handleRollClick = () => {
+		let randomDieOne = sides[Math.floor(Math.random() * sides.length)];
+		let randomDieTwo = sides[Math.floor(Math.random() * sides.length)];
+		setDice({ dieOne: randomDieOne, dieTwo: randomDieTwo, rolling: true });
 		setTimeout(() => {
-			setRolling(false);
-		}, 1000);
+			setDice({ dieOne: "one", dieTwo: "one", rolling: false });
+		}, 1200);
 	};
 
 	return (
 		<div className="RollDice">
 			<div className="RollDice-container">
-				<Die face={die1} rolling={rolling} />
-				<Die face={die2} rolling={rolling} />
+				<Die face={dice.dieOne} rolling={dice.rolling} />
+				<Die face={dice.dieTwo} rolling={dice.rolling} />
 			</div>
-			<button onClick={roll}>{rolling ? "Rolling..." : "Roll Dice!"}</button>
+			<button onClick={handleRollClick}>
+				{dice.rolling ? "Rolling..." : "Roll Dice!"}
+			</button>
 		</div>
 	);
+	
 };
